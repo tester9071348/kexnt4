7760bd77dd622dfb60911091f48abeca7d238253
tester9071348 <tester9071348@outlook.com>
2025-02-16 14:19:09 +0300

    Update user32.c

--- user32.c
+++ user32.c
@@ -1,158 +1,71 @@
-INT APIENTRY NtUserEnumDisplayMonitors(
-    OPTIONAL IN HDC hdc,
-    OPTIONAL IN LPCRECTL pUnsafeRect,
-    OPTIONAL OUT HMONITOR *phUnsafeMonitorList,
-    OPTIONAL OUT PRECTL prcUnsafeMonitorList,
-    OPTIONAL IN DWORD dwListSize)
+BOOL WINAPI EnumDisplayMonitors(
+    HDC hdc,
+    LPCRECT lprcClip,
+    MONITORENUMPROC lpfnEnum,
+    LPARAM dwData)
 {
-    UINT cMonitors, i;
-    INT iRet = -1;
-    HMONITOR *phMonitorList = NULL;
-    PRECTL prcMonitorList = NULL;
-    RECTL rc, *pRect;
-    RECTL DcRect = {0};
-    NTSTATUS Status;
+    INT iCount, i;
+    HMONITOR *hMonitorList;
+    LPRECT pRectList;
+    HANDLE hHeap;
+    BOOL ret = FALSE;
 
-    /* Get rectangle */
-    if (pUnsafeRect != NULL)
+    /* get list of monitors/rects */
+    iCount = NtUserEnumDisplayMonitors(hdc, lprcClip, NULL, NULL, 0);
+    if (iCount < 0)
     {
-        Status = MmCopyFromCaller(&rc, pUnsafeRect, sizeof(RECT));
-        if (!NT_SUCCESS(Status))
-        {
-            TRACE("MmCopyFromCaller() failed!\n");
-            SetLastNtError(Status);
-            return -1;
-        }
+        /* FIXME: SetLastError() */
+        return FALSE;
+    }
+    if (iCount == 0)
+    {
+        return TRUE;
     }
 
-    if (hdc != NULL)
+    hHeap = GetProcessHeap();
+    hMonitorList = HeapAlloc(hHeap, 0, sizeof (HMONITOR) * iCount);
+    if (hMonitorList == NULL)
     {
-        PDC pDc;
-        INT iRgnType;
-
-        /* Get visible region bounding rect */
-        pDc = DC_LockDc(hdc);
-        if (pDc == NULL)
-        {
-            TRACE("DC_LockDc() failed!\n");
-            /* FIXME: setlasterror? */
-            return -1;
-        }
-        iRgnType = REGION_GetRgnBox(pDc->prgnVis, &DcRect);
-        DC_UnlockDc(pDc);
-
-        if (iRgnType == 0)
-        {
-            TRACE("NtGdiGetRgnBox() failed!\n");
-            return -1;
-        }
-        if (iRgnType == NULLREGION)
-            return 0;
-        if (iRgnType == COMPLEXREGION)
-        {
-            /* TODO: Warning */
-        }
-
-        /* If hdc and pRect are given the area of interest is pRect with
-           coordinate origin at the DC position */
-        if (pUnsafeRect != NULL)
-        {
-            rc.left += DcRect.left;
-            rc.right += DcRect.left;
-            rc.top += DcRect.top;
-            rc.bottom += DcRect.top;
-        }
-        /* If hdc is given and pRect is not the area of interest is the
-           bounding rect of hdc */
-        else
-        {
-            rc = DcRect;
-        }
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+        return FALSE;
     }
-
-    if (hdc == NULL && pUnsafeRect == NULL)
-        pRect = NULL;
-    else
-        pRect = &rc;
-
-    UserEnterShared();
-
-    /* Find intersecting monitors */
-    cMonitors = IntGetMonitorsFromRect(pRect, NULL, NULL, 0, MONITOR_DEFAULTTONULL);
-    if (cMonitors == 0 || dwListSize == 0 ||
-        (phUnsafeMonitorList == NULL && prcUnsafeMonitorList == NULL))
+    pRectList = HeapAlloc(hHeap, 0, sizeof (RECT) * iCount);
+    if (pRectList == NULL)
     {
-        /* Simple case - just return monitors count */
-        TRACE("cMonitors = %u\n", cMonitors);
-        iRet = cMonitors;
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
         goto cleanup;
     }
 
-    /* Allocate safe buffers */
-    if (phUnsafeMonitorList != NULL && dwListSize != 0)
+    iCount = NtUserEnumDisplayMonitors(hdc, lprcClip, hMonitorList, pRectList, iCount);
+    if (iCount <= 0)
     {
-        phMonitorList = ExAllocatePoolWithTag(PagedPool, sizeof (HMONITOR) * dwListSize, USERTAG_MONITORRECTS);
-        if (phMonitorList == NULL)
-        {
-            EngSetLastError(ERROR_NOT_ENOUGH_MEMORY);
-            goto cleanup;
-        }
-    }
-    if (prcUnsafeMonitorList != NULL && dwListSize != 0)
-    {
-        prcMonitorList = ExAllocatePoolWithTag(PagedPool, sizeof(RECT) * dwListSize,USERTAG_MONITORRECTS);
-        if (prcMonitorList == NULL)
-        {
-            EngSetLastError(ERROR_NOT_ENOUGH_MEMORY);
-            goto cleanup;
-        }
+        /* FIXME: SetLastError() */
+        goto cleanup;
     }
 
-    /* Get intersecting monitors */
-    cMonitors = IntGetMonitorsFromRect(pRect, phMonitorList, prcMonitorList,
-                                       dwListSize, MONITOR_DEFAULTTONULL);
-
-    if (hdc != NULL && pRect != NULL && prcMonitorList != NULL)
+    /* enumerate list */
+    for (i = 0; i < iCount; i++)
     {
-        for (i = 0; i < min(cMonitors, dwListSize); i++)
-        {
-            _Analysis_assume_(i < dwListSize);
-            prcMonitorList[i].left -= DcRect.left;
-            prcMonitorList[i].right -= DcRect.left;
-            prcMonitorList[i].top -= DcRect.top;
-            prcMonitorList[i].bottom -= DcRect.top;
-        }
-    }
+        HMONITOR hMonitor = hMonitorList[i];
+        LPRECT pMonitorRect = pRectList + i;
+        HDC hMonitorDC = NULL;
 
-    /* Output result */
-    if (phUnsafeMonitorList != NULL && dwListSize != 0)
-    {
-        Status = MmCopyToCaller(phUnsafeMonitorList, phMonitorList, sizeof(HMONITOR) * dwListSize);
-        if (!NT_SUCCESS(Status))
+        if (hdc != NULL)
         {
-            SetLastNtError(Status);
-            goto cleanup;
+            /* make monitor DC */
+            hMonitorDC = hdc;
         }
-    }
-    if (prcUnsafeMonitorList != NULL && dwListSize != 0)
-    {
-        Status = MmCopyToCaller(prcUnsafeMonitorList, prcMonitorList, sizeof(RECT) * dwListSize);
-        if (!NT_SUCCESS(Status))
-        {
-            SetLastNtError(Status);
-            goto cleanup;
-        }
-    }
 
-    /* Return monitors count on success */
-    iRet = cMonitors;
-
+        if (!lpfnEnum(hMonitor, hMonitorDC, pMonitorRect, dwData))
+            goto cleanup; /* return FALSE */
+    }
+    
+    ret = TRUE;
+    
 cleanup:
-    if (phMonitorList)
-        ExFreePoolWithTag(phMonitorList, USERTAG_MONITORRECTS);
-    if (prcMonitorList)
-        ExFreePoolWithTag(prcMonitorList, USERTAG_MONITORRECTS);
-
-    UserLeave();
-    return iRet;
+    if(hMonitorList)
+        HeapFree(hHeap, 0, hMonitorList);
+    if(pRectList)
+        HeapFree(hHeap, 0, pRectList);
+    return ret;
 }
